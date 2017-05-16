bd82f80c51015f916d7b0cf941c1e57142724851
TakLee96 <jiahang_li@outlook.com>
2017-05-16 12:51:29 -0700

    refactor code to start python deep learning project

---
+++ python/gomoku.py
@@ -0,0 +1,44 @@
+import numpy as np
+
+N = 15  # board size
+DIRECTIONS = [(1, 0), (0, 1), (1, 1), (1, -1)]
+
+class State:
+  def __init__(self):
+    self.player = 1  # 1 for black and -1 for white
+    self.board = np.zeros(shape=(N, N), dtype=np.int8)
+    self.history = list()
+    self.end = False
+
+  def _count(self, x, y, dx, dy):
+    if x >= 15 or x < 0 or y >= 15 or y < 0 or self.board[x, y] != self.player:
+      return 0
+    return 1 + self._count(x + dx, y + dy, dx, dy)
+
+  def _win(self, x, y):
+    for dx, dy in DIRECTIONS:
+      if self._count(x + dx, y + dy, dx, dy) + \
+        self._count(x - dx, y - dy, -dx, -dy) >= 4:
+        return True
+    return False
+
+  def move(self, x, y):
+    assert self.board[x, y] == 0 and not self.end
+    self.board[x, y] = self.player
+    self.history.append((x, y))
+    if self._win(x, y):
+      self.end = True
+    else:
+      self.player = -self.player
+    return self.end
+
+  def rewind(self):
+    x, y = history.pop()
+    self.board[x, y] = 0
+    if self.end:
+      self.end = False
+    else:
+      self.player = -self.player
+
+  def __str__(self):
+    return str(self.board).replace("-1", "x").replace(" 1", "o").replace(" 0", "+")
---
+++ python/minimax.py
@@ -0,0 +1,50 @@
+
+class MinimaxAgent:
+  def __init__(self, player, policy, value, depth=5)
+    self.player = player
+    self.policy = policy  # a function
+    self.value = value    # a function
+    self.depth = depth
+
+  def _max_value(self, state, alpha, beta, depth):
+    assert self.player == state.player
+    if state.end:
+      return 0.0
+    if depth == self.depth:
+      return self.value(state.player * state.board)
+    max_value = 0.0
+    for x, y in self.policy(state.player * state.board):
+      state.move(x, y)
+      max_value = max(max_value, self._min_value(state, alpha, beta, depth + 1))
+      if max_value > beta:
+        return max_value
+      alpha = max(alpha, max_value)
+      state.rewind()
+    return max_value
+
+  def _min_value(self, state, alpha, beta, depth):
+    assert self.player != state.player
+    if state.end:
+      return 1.0
+    if depth == self.depth:
+      return 1.0 - self.value(state.player * state.board)
+    min_value = 1.0
+    for x, y in self.policy(state.player * state.board):
+      state.move(x, y)
+      min_value = min(min_value, self._max_value(state, alpha, beta, depth + 1))
+      if min_value < alpha:
+        return min_value
+      beta = min(beta, min_value)
+      state.rewind()
+    return min_value
+
+  def get_action(self, state):
+    assert self.player == state.player
+    actions = self.policy(state.player * state.board)
+    def key_func(action):
+      x, y = action
+      state.move(x, y)
+      val = self._min_value(state, 0.0, 1.0, 1)
+      state.rewind()
+      return val
+    return max(actions, key=key_func)
