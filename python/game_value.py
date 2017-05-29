789fdf9430296f83b217e851d4acb94d34445fd5
TakLee96 <jiahang_li@outlook.com>
2017-05-29 23:51:37 +0800

    experimenting qbtnet

--- python/game_value.py
+++ python/game_value.py
@@ -1,14 +1,15 @@
 """ battle between agent and human """
-import tkinter as tk
 import numpy as np
+import tkinter as tk
 import tensorflow as tf
-from os import path, system
+import multiprocessing as mp
+from os import path
 from state import State
 from feature import diff
 
 
 class Application(tk.Frame):
-    def __init__(self, session, master):
+    def __init__(self, state_queue, value_queue, master):
         tk.Frame.__init__(self, master)
         self.button = list()
         self.frames = list()
@@ -19,15 +20,27 @@ class Application(tk.Frame):
           tk.PhotoImage(file=path.join(root, "naught.gif")),
           tk.PhotoImage(file=path.join(root, "cross.gif")),
         ]
-        self.session = session
+        self.state_queue = state_queue
+        self.value_queue = value_queue
         self.pack()
         self.create_widgets()
         self.draw_probability()
 
     def draw_probability(self):
-        if self.state.player == 1:
-            y = self.session.run("y:0", feed_dict={"x:0": self.state.board.reshape((1, 225)), "y_:0": np.zeros(shape=(1, 1))})
-            print("winning probability for black: {0}%".format((y[0, 0] + 1) * 50))
+        # for i in range(15):
+        #     for j in range(15):
+        #         if self.state.board[i, j] == 0:
+        #             new = diff(self.state, i, j)
+        #             if new["-o-oo-"] + new["-ooo-"] >= 2 or \
+        #                 new["four-o"] + new["-oooo-"] >= 2 or self.state._long(i, j):
+        #                 self.button[i * 15 + j].config(image="", text="%.2f" % -1)
+        #             else:
+        #                 self.state.move(i, j)
+        #                 self.state_queue[self.state.player].put(self.state)
+        #                 self.button[i * 15 + j].config(image="", text="%.2f" % self.value_queue.get())
+        #                 self.state.rewind()
+        self.state_queue[self.state.player].put(self.state)
+        print(self.value_queue.get()[0, 0])
 
     def highlight(self, x, y):
         for i, j in self.state.highlight(x, y):
@@ -54,24 +67,47 @@ class Application(tk.Frame):
                 f.pack_propagate(0)
                 f.grid(row=i, column=j, padx=0, pady=0)
                 self.frames.append(f)
-                b = tk.Label(f, image=self.image[0], bg="red")
+                b = tk.Label(f, image=self.image[0], bg="yellow")
                 b.pack(fill=tk.BOTH, expand=1)
                 b.bind("<Button-1>", self.click(i, j))
                 self.button.append(b)
 
 
+class Agent(mp.Process):
+    def __init__(self, state_queue, value_queue, name):
+        mp.Process.__init__(self)
+        self.state_queue = state_queue
+        self.value_queue = value_queue
+        self.name = name
+
+    def run(self):
+        with tf.Session() as session:
+            root = path.join(path.dirname(__file__), "model", "value", self.name)
+            saver = tf.train.import_meta_graph(path.join(root, self.name + ".meta"), clear_devices=True)
+            saver.restore(session, tf.train.latest_checkpoint(root))
+            while True:
+                state = self.state_queue.get()
+                if state is None:
+                    return
+                y = session.run("y:0", feed_dict={
+                    "x:0": state.board.reshape((1, 225)),
+                    "y_:0": np.zeros(shape=(1, 1))})
+                self.value_queue.put(y)
+
+
 def run():
-    with tf.Session() as session:
-        name = "qbtnet-black"
-        checkpoint = 4000
-        root = path.join(path.dirname(__file__), "model", "value", name)
-        saver = tf.train.import_meta_graph(path.join(root, name + ".meta"), clear_devices=True)
-        saver.restore(session, path.join(root, name + "-" + str(checkpoint)))
-        root = tk.Tk()
-        root.wm_title("Alpha Gomoku")
-        root.attributes("-topmost", True)
-        app = Application(session, root)
-        app.mainloop()
+    states = [None, mp.Queue(), mp.Queue()]
+    values = mp.Queue()
+    players = [Agent(states[1], values, "qbtnet-black"), Agent(states[-1], values, "qbtnet-white")]
+    for player in players:
+        player.start()
+    root = tk.Tk()
+    root.wm_title("Alpha Gomoku")
+    root.attributes("-topmost", True)
+    app = Application(states, values, root)
+    app.mainloop()
+    states[1].put(None)
+    states[-1].put(None)
 
 
 run()
